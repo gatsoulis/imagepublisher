de88edd64993ed3a11117685413c7af031d7202f
yianni@robotlab7 <i.gatsoulis@ulster.ac.uk>
2010-11-04 13:33:53 +0000

    OpenCV load images.

--- src/main.cpp
+++ src/main.cpp
@@ -7,9 +7,16 @@
  * [Note: deprecated practice as git will maintain author info and blame logs.]
  */
 
+#define BOOST_FILESYSTEM_NO_DEPRECATED
+
 #include "boost/filesystem.hpp"
 #include <iostream>
+#include <string>
 #include <vector>
+#include <algorithm>
+#include <opencv/cv.h>
+#include <opencv/highgui.h>
+
 using namespace boost::filesystem;
 using namespace std;
 
@@ -18,20 +25,31 @@ using namespace std;
 int main(int argc, char *argv[])
 {
 	string p(argc <= 1 ? "." : argv[1]);
-	vector<string> filenames;
+	vector<string> filenames;	IplImage *img = NULL;
 
 	if (is_directory(p)) {
 		for (directory_iterator itr(p); itr!=directory_iterator(); ++itr)
 			if (is_regular_file(itr->status())) {
-				string str(itr->path().filename());
+				string str(itr->path().file_string());
 				filenames.push_back(str);
 			}
 	} else
 		cout << "Enter directory name or leave blank for current directory" << endl;
 
+	sort(filenames.begin(), filenames.end());
+	cvNamedWindow("main", CV_WINDOW_AUTOSIZE);
+	cvMoveWindow("main", 100, 100);
 	for (vector<string>::iterator itr = filenames.begin(); itr != filenames.end(); ++itr) {
-		cout << *itr << endl;
+		cout << itr->c_str() << endl;
+		img = cvLoadImage(itr->c_str());
+		if(!img)
+			cout << "Could not load image file: " << itr->c_str() << endl;
+		else {
+			cvShowImage("main", img);
+			cvWaitKey(0);
+		}
 	}
+	cvReleaseImage(&img );
 
 	return 0;
 }
