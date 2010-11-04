ae6c80ecffadbd812239739dde37031e17dbe84e
yianni@robotlab7 <i.gatsoulis@ulster.ac.uk>
2010-11-04 14:46:41 +0000

    Added ROS wrap. Done.

--- src/main.cpp
+++ src/main.cpp
@@ -10,20 +10,37 @@
 #define BOOST_FILESYSTEM_NO_DEPRECATED
 
 #include "boost/filesystem.hpp"
+#include "sensor_msgs/Image.h"
+#include "image_transport/image_transport.h"
+#include "cv_bridge/CvBridge.h"
+
 #include <iostream>
 #include <string>
 #include <vector>
 #include <algorithm>
 #include <opencv/cv.h>
 #include <opencv/highgui.h>
+#include <ros/ros.h>
+#include <signal.h>
 
 using namespace boost::filesystem;
 using namespace std;
 
-
+void ctrlc(int s)
+{
+	cout << "ctrl-c pressed" << endl;
+	ros::shutdown();
+	signal(SIGINT, SIG_DFL);
+}
 
 int main(int argc, char *argv[])
 {
+	ros::init(argc, argv, "StaticImagePublisher");
+	ros::NodeHandle n;
+	image_transport::ImageTransport it_(n);
+	sensor_msgs::CvBridge bridge_;
+	image_transport::Publisher image_pub_;
+	image_pub_ = it_.advertise("/camera/image_raw", 1);
 	string p(argc <= 1 ? "." : argv[1]);
 	vector<string> filenames;	IplImage *img = NULL;
 
@@ -38,18 +55,26 @@ int main(int argc, char *argv[])
 
 	sort(filenames.begin(), filenames.end());
 	cvNamedWindow("main", CV_WINDOW_AUTOSIZE);
-	cvMoveWindow("main", 100, 100);
-	for (vector<string>::iterator itr = filenames.begin(); itr != filenames.end(); ++itr) {
-		cout << itr->c_str() << endl;
-		img = cvLoadImage(itr->c_str());
-		if(!img)
-			cout << "Could not load image file: " << itr->c_str() << endl;
-		else {
-			cvShowImage("main", img);
-			cvWaitKey(0);
+	cvMoveWindow("main", 100, 600);
+	signal(SIGINT, ctrlc);
+	for(;;) {
+		for (vector<string>::iterator itr = filenames.begin(); itr != filenames.end(); ++itr) {
+			img = cvLoadImage(itr->c_str());
+			if(!img)
+				cout << "Could not load image file: " << itr->c_str() << endl;
+			else {
+				cvShowImage("main", img);
+				cvWaitKey(300);
+				try	{
+					image_pub_.publish(bridge_.cvToImgMsg(img, "bgr8"));
+				} catch (sensor_msgs::CvBridgeException error) {
+					ROS_ERROR("error");
+				}
+				cvReleaseImage(&img);
+			}
 		}
 	}
-	cvReleaseImage(&img );
 
+	ros::spin();
 	return 0;
 }
